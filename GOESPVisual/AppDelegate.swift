91cbc54d6289afde81787e6ce551215d73025c89
Olga Vyrostko <violg@yandex-team.ru>
2018-11-10 18:40:43 +0300

    add: basic structure

---
+++ GOESPVisual/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GOESPVisual
+//
+//  Created by Olga Vyrostko on 10/11/2018.
+//  Copyright © 2018 olgavyrostko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GOESPVisual/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GOESPVisual
+//
+//  Created by Olga Vyrostko on 10/11/2018.
+//  Copyright © 2018 olgavyrostko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
